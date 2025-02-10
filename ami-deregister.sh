7328d042bcd6347eee5569efdaf3a945ba2d69fb
Adarlan Teixeira <adarlanteixeira@outlook.com>
2025-02-09 21:29:16 -0300

    Add script to remove EBS snapshots

--- ami-deregister.sh
+++ ami-deregister.sh
@@ -2,21 +2,37 @@
 set -e
 cd $(dirname $0)
 
+aws_region=us-east-1
+
+echo "[INFO] Fetching AMI IDs associated to this project"
 ami_ids=$(aws ec2 describe-images \
-    --region us-east-1 \
+    --region $aws_region \
     --owners self \
     --filters "Name=name,Values=my-k8s-ami-*" \
-    --query 'Images[*].ImageId' \
-    --output text
+    --query 'Images[*].ImageId'
 )
+echo "$ami_ids" | jq
 
-for ami_id in $ami_ids; do
-    echo "Deregistering AMI: $ami_id"
+echo "$ami_ids" | jq -r '.[]' | while read -r ami_id; do
+
+    echo "[INFO] Fetching EBS snapshot IDs associated to AMI: $ami_id"
+    ebs_snapshot_ids=$(aws ec2 describe-images \
+        --region $aws_region \
+        --image-ids $ami_id \
+        --query "Images[0].BlockDeviceMappings[*].Ebs.SnapshotId"
+    )
+    echo "$ebs_snapshot_ids" | jq
+
+    echo "[INFO] Deregistering AMI: $ami_id"
     aws ec2 deregister-image \
-        --region us-east-1 \
+        --region $aws_region \
         --image-id $ami_id
-    # TODO Delete EBS snapshots created by CreateImage
-    # Standard EBS snapshot storage pricing: $0.05/GB-month
-    # These snapshots contain the following description:
-    # "Created by CreateImage(INSTANCE_ID) for AMI_ID"
+
+    echo "$ebs_snapshot_ids" | jq -r '.[]' | while read -r ebs_snapshot_id; do
+
+        echo "[INFO] Deleting EBS snapshot: $ebs_snapshot_id"
+        aws ec2 delete-snapshot \
+            --region $aws_region \
+            --snapshot-id $ebs_snapshot_id
+    done
 done
